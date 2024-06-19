e84b1f789f3a77431cfa8bec06ddb3dd3992c7bd
Jason Tennant <jason.tennant@outlook.com>
2024-06-19 06:55:48 -0700

    code updates

--- Chapter03/geo2json.c
+++ Chapter03/geo2json.c
@@ -15,6 +15,17 @@ int main()
 		else
 			started = 1;
 
+		if (latitude < -90 || latitude > 90)
+		{
+			fprintf(stderr, "Invalid latitude %f\n", latitude);
+			exit(2);
+		}
+		if (longitude < -180 || longitude > 180)
+		{
+			fprintf(stderr, "Invalid longitude %f\n", longitude);
+			exit(2);
+		}
+
 		printf("{latitude: %f, longitude: %f, info: '%s'}", latitude, longitude, info);
 	}
 	puts("\n]");
